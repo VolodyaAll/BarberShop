1566026870263d918f6732cb005fd67c8b2ea0fc
VolodyaAll <volodya22all@gmail.com>
2019-06-22 21:42:59 +0300

    app.rb refactoring

--- app.rb
+++ app.rb
@@ -66,9 +66,9 @@ post '/visit' do
   @color = params[:color]
   @db = get_db
 
-  hh = {:username => 'Введите имя',
-    :phone => 'Введите телефон',
-    :date_time => 'Введите дату и время'}
+  hh = {username: 'Введите имя',
+    phone: 'Введите телефон',
+    date_time: 'Введите дату и время'}
 
   @error = hh.select{|key,_| params[key] == ""}.values.join(", ")
 
@@ -95,8 +95,8 @@ end
 post '/contacts' do
   @email = params["email"]
   @message = params["message"]
-  hh = {:email => 'Введите Email',
-    :message => 'Введите сообщение'}
+  hh = {email: 'Введите Email',
+    message: 'Введите сообщение'}
 
   @error = hh.select{|key,_| params[key] == ""}.values.join(", ")
 
@@ -105,16 +105,16 @@ post '/contacts' do
     f.write("Email: #{params[:email]}, Message: #{params[:message]}.\n")
     f.close
 
-    Pony.mail(:to => 'sharaivladimir2@gmail.com', :from => @email, :body => @message,
-      :via => :smtp,
-      :via_options => {
-      :address              => 'smtp.gmail.com',
-      :port                 => '587',
-      :enable_starttls_auto => true,
-      :user_name            => 'sharaivladimir2@gmail.com',
-      :password             => 'Your_password',
-      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-      :domain               => "localhost.localdomain"})
+    Pony.mail(to: 'sharaivladimir2@gmail.com', from: @email, body: @message,
+      via: :smtp,
+      via_options: {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: 'sharaivladimir2@gmail.com',
+      password: 'Your_password',
+      authentication: :plain, # :plain, :login, :cram_md5, no auth by default
+      domain: "localhost.localdomain"})
 
     erb "We will send our answer to #{params[:email]}. See you! <a href=\"http://localhost:4567\">На главную</a>"
   else
